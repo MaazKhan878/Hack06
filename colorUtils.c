80b1ea7a0671b0c1c9c4b911b1d7e7291c23b073
Maaz_Khan <maazkhan0348810@gmail.com>
2023-12-19 03:10:08 +0500

    add comment make some cleaity etc

--- colorUtils.c
+++ colorUtils.c
@@ -1,24 +1,31 @@
 #include<stdio.h>
 #include<math.h>
 #include"colorUtils.h"
+// made function to find the max 
 double max(double red , double green, double blue){
     double max = (red > green)? red : green;
     return (max > blue)? max : blue;
 }
+// Function: to convert the rgb value to scale b/w 0,1
 double scale(int r){
     return (double)r/255.0;
 } 
+// Function: which convert the rgb Filter to CMYK filter
 int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k){
+    // ERROR HANDLING: to check the input is valid or not
     if(r>255 || r<0 ||g>255 || g<0 ||b>255 || b<0){
         return 1;
     }
+    // convert to 0,1 scale
     double red = scale(r);
     double green = scale(g);
     double blue = scale(b);
+    // FORMULA: TO convert the filter from rgb to CMYK
     *k = 1 - max(red,green,blue);
     *c = (1 - r - *k)/(1 - *k);
     *m = (1 - g - *k)/(1 - *k);
     *y = (1 - b - *k)/(1 - *k);
+    // valid Input Return 0;
     return 0;
 }
 
